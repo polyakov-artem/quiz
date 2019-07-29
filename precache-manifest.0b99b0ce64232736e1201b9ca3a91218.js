54a3552666cc509ae9cbdc5e8b72d6f7a1bf59e5
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2019-07-29 22:39:08 +0400

    add files

---
+++ precache-manifest.0b99b0ce64232736e1201b9ca3a91218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4531a420020dca79e823",
+    "url": "/quiz/static/js/runtime~main.991ea3ce.js"
+  },
+  {
+    "revision": "40bc3fd85db97cb25dec",
+    "url": "/quiz/static/js/main.526e663c.chunk.js"
+  },
+  {
+    "revision": "c9612533a4c5ff562ec4",
+    "url": "/quiz/static/js/2.3e90781b.chunk.js"
+  },
+  {
+    "revision": "40bc3fd85db97cb25dec",
+    "url": "/quiz/static/css/main.05dbbd64.chunk.css"
+  },
+  {
+    "revision": "bdea4a2cf6baa9c6c2a7342941b87786",
+    "url": "/quiz/index.html"
+  }
+];
